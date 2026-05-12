0774c5e4a580bd6568c6e3794a594137c7299396
yogita <yogikohli7100@gmail.com>
2026-05-12 17:29:34 +0530

    cors

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI||"mongodb+srv://yogita:yogita@cluster0.vbuxtwd.mongodb.net/?appName=Cluster0");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
